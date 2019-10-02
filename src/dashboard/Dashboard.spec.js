3b709329140f30b059e5cd37f0f83e8954af8398
MicahJank <MicahJank@users.noreply.github.com>
2019-10-02 13:31:38 -0500

    did testing for the dashboard

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
-// Test away
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Dashboard from './Dashboard.js'
+
+
+test("Dashboard should show both the controls and the display", () => {
+    const { getByTestId } = render(<Dashboard />);
+
+    expect(getByTestId('displayPanel')).toBeDefined();
+    expect(getByTestId('controlPanel')).toBeDefined(); 
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,51 @@
-// Test away!
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import Display from "./Display.js";
+import Controls from '../controls/Controls.js';
+
+test("default rendering should be unlocked and open", () => {
+    const mockState = {
+        closed: false,
+        locked: false
+    }
+
+    const { getByText } = render(<Display
+            locked={mockState.locked}
+            closed={mockState.closed}
+            />);
+
+    expect(getByText(/unlocked/i)).toBeDefined();
+    expect(getByText(/open/i)).toBeDefined();
+});
+
+
+test("gate cannot be open if it is locked(i.e. the button should be disabled", () => {
+    const mockToggleClosed = jest.fn();
+    const { getByText } = render(<Controls 
+                        locked={true}
+                        closed={true}
+                        toggleClosed={mockToggleClosed}
+                        />)
+    
+    const openGateButton = getByText(/open gate/i);
+
+    fireEvent.click(openGateButton);
+
+    expect(mockToggleClosed).not.toHaveBeenCalled(); // since locked is true, the fireEvent should not work
+});
+
+test("gate cannot be closed if it is locked(i.e. the button should be disabled", () => {
+    const mockToggleClosed = jest.fn();
+    const { getByText } = render(<Controls 
+                        locked={true}
+                        closed={false}
+                        toggleClosed={mockToggleClosed}
+                        />)
+    
+    const closeGateButton = getByText(/close gate/i);
+
+    fireEvent.click(closeGateButton);
+
+    expect(mockToggleClosed).not.toHaveBeenCalled(); // since locked is true, the fireEvent should not work
+})
