0854b18927b2039f4bbf52da5020d10fa8281314
MicahJank <MicahJank@users.noreply.github.com>
2019-10-02 19:42:50 -0500

    finished the display testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -68,5 +68,44 @@ test("if it displays Closed when the closed prop is true and Open otherwise", ()
     }
     const { getByText } = render(<Display closed={mockState.closed} />); 
 
-    expect(getByText(mockState.closed ? /closed/i : /open/i)).toBeDefined();
+    expect(getByText(mockState.closed ? /closed/i : /open/i)).toBeDefined(); 
+});
+
+test("if it displays locked when the locked prop is true and unlocked otherwise", () => {
+    const mockState = {
+        locked: false
+    }
+    const { getByText } = render(<Display locked={mockState.locked} />); 
+
+    expect(getByText(mockState.locked ? /locked/i : /unlocked/i)).toBeDefined();
+})
+
+test("when gate is locked or closed use the red-led class", () => {
+    const mockState = {
+        locked: true,
+        closed: true
+    }
+
+    const { getByText } = render(<Display locked={mockState.locked} closed={mockState.closed} />)
+    
+    const gateLocked = getByText(/locked/i);
+    expect(gateLocked.classList.contains('red-led')).toBe(true);
+    
+    const gateClosed = getByText(/closed/i);
+    expect(gateClosed.classList.contains('red-led')).toBe(true); 
+});
+
+test("when gate is unlocked or open use the green-led class", () => {
+    const mockState = {
+        locked: false,
+        closed: false
+    }
+
+    const { getByText } = render(<Display locked={mockState.locked} closed={mockState.closed} />)
+
+    const gateUnlocked = getByText(/unlocked/i);
+    expect(gateUnlocked.classList.contains('green-led')).toBe(true);
+
+    const gateOpen = getByText(/open/i);
+    expect(gateOpen.classList.contains('green-led')).toBe(true);
 })
